617d4d54a50f0250d0f6be8da0fb86573b7968a9
Devin Kopp <arkidillo@gmail.com>
2017-03-30 21:44:38 -0400

    Code fully written, but probably not working

--- net.cpp
+++ net.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 using namespace std;
 /*#include "net.h"
 
@@ -14,22 +15,21 @@ Net::Net(void){
 }
 */
 double sigmoid(double x);
+double errorSigmoid(double x);
 
 int main(){
-
-
 	char plainText[10];
 	cout << "Enter plain text up to 10 characters:";
 	cin >> plainText;
 
-   char ecryptedText[10];
-   cout << "Enter the encrypted text:";
-   cin >> ecryptedText;
+	char encryptedText[10];
+	cout << "Enter the encrypted text:";
+	cin >> encryptedText;
 	srand(time(NULL));
 
 	/* Input/ ouput nodes will be turned on according to the binary value of the ascii value of each letter */
 	int inputs[7];
-   int target[7];
+	int target[7];
 	/* syn0 are the edges connecting the input nodes and the hidden layer */
 	double syn0[7][10];
 	double hiddenNode[10];
@@ -37,7 +37,6 @@ int main(){
 	double syn1[10][7];
 	/* The ouputs will be doubles, but will be cast to ints as the output, to get the ascii value */
 	double outNode[7];
-   double finalError[7];
 
 	/* Mask will mast out each bit of the input string to get which bits of the input nodes should be turned on */
 	char mask;
@@ -50,14 +49,16 @@ int main(){
 		}
 	}
 
+	cout << strlen(encryptedText);
+	for (int z = 0; z < 100; z++){
 	/* Main loop that goes through each character at a time. */
-	for (int i = 0; i < 10; i++){
+	for (int i = 0; i < strlen(encryptedText); i++){
 
 		/* For each input character, we need to mask out the bits to see what inputs will be on or off */
 		mask = 1;
 		for (int j = 0; j < 7; j++){	
 			inputs[j] = (plainText[i] & mask) == 0 ? 0 : 1;
-         target[j] = (encryptedText[i] & mask) == 0 ? 0 : 1;
+         	target[j] = (encryptedText[i] & mask) == 0 ? 0 : 1;
 			mask = mask << 1;	//Shift mask over 1 to check for each bit 
 		}
 
@@ -68,28 +69,70 @@ int main(){
 			for (int k = 0; k < 7; k++){
 				hiddenNode[j] += inputs[k] * syn0[k][j];
 			}
-         
-         /* Apply sigmoid function to hiddenlayer sums */
-         hiddenNode[j] = sigmoid(hiddenNode[j]);
-		}
+         }
 
 		/* Iterates to find outputs */
 		for (int j = 0; j < 7; j++){
 			/* Iterates through all synapses connecting to the jth output node */
 			for (int k = 0; k < 10; k++){
-				outNode[j] += hiddenNode[k] * syn1[k][j];
+				outNode[j] += sigmoid(hiddenNode[k]) * syn1[k][j];
 			}
 		}
       
-      /* Outputs are now calculated by this point. We now need to check against the target */
-      for (int j = 0; j < 7; j++){
-         finalError[j] = target[j] - outNode[j];
-      }
+     	 double finalError[7];
+      	/* Outputs are now calculated by this point. We now need to check against the target */
+      	for (int j = 0; j < 7; j++){
+        	finalError[j] = (target[j] - sigmoid(outNode[j])) * errorSigmoid(outNode[j]);
+      	}
+
+      	double newSyn1[10][7];
+      	/* For every synapse, we must now change the weight to be finalError for its output node, times the hiddenLayer result and add that to the old synapse value. */
+      	for (int j = 0; j < 7; j++){
+      		for (int k = 0; k < 10; k++){
+      			newSyn1[k][j] = syn1[k][j] + sigmoid(hiddenNode[k]) * finalError[j]; 
+      		}
+      	}
+
+      	double deltaHiddenSum[10][7];
+      	/* Calculates the change in hidden sum by the last value */
+      	for (int j = 0; j < 7; j++){
+      		for (int k = 0; k < 10; k++){
+      			deltaHiddenSum[k][j] = finalError[j] * syn1[k][j] * errorSigmoid(hiddenNode[k]);
+      		}
+      	}
+
+      	/* Set the new weights of the first synapses (input to hidden) */
+      	for (int j = 0; j < 7; j++){
+      		for (int k = 0; k < 10; k++){
+      			syn0[j][k] += deltaHiddenSum[k][j] * inputs[j];
+      		}
+      	}
+
+      	/* Now set syn1 to the new weights */
+      	for (int j = 0; j < 10; j++){
+      		for (int k = 0; k < 7; k++){
+      			syn1[j][k] = newSyn1[j][k];
+      		}
+      	}
+
+      	char outValue;
+      	/* Now, convert output back to ascii value */
+      	for (int j = 0; j < 7; j++){
+      		outValue += outNode[j] * pow(2, j);
+      	}
+      	cout << outValue;
 	}
+
+	cout << endl;
+}
 	return 0;
 
 }
 
 double sigmoid(double x){
-	return 1/(1 + pow(2.71828183, -1*x);
+	return 1.0/(1.0 + exp(-1*x));
+}
+
+double errorSigmoid(double x){
+	return exp(x)/pow((exp(x) + 1), 2.0);
 }
